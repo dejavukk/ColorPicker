8f7dc919e059e0b8d94f9abf26784e42ce85a4a9
junhyuk <dejavukk1@gmail.com>
2019-12-28 16:37:25 +0900

    뷰의 색상 변환 메소드 및 슬라이더 기능 구현중.

--- ColorPicker/ViewController.swift
+++ ColorPicker/ViewController.swift
@@ -9,12 +9,66 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // View Property.
+    @IBOutlet weak var colorView: UIView!
+    
+    // Switch Properties.
+    @IBOutlet weak var greenSwitch: UISwitch!
+    @IBOutlet weak var blueSwitch: UISwitch!
+    @IBOutlet weak var redSwitch: UISwitch!
+    
+    // Slider Properties.
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // colorView.backgroundColor = .black
+        updateColor()
     }
-
+    
+    // 뷰 화면 색상 변경 메소드.
+    @IBAction func switchChanged(_ sender: UISwitch) {
+        
+        if sender.isOn {
+            colorView.backgroundColor = .red
+        } else {
+            colorView.backgroundColor = .black
+        }
+    }
+    
+    // 컬러 변환 메소드 구현.
+    func updateColor() {
+        
+        var red: CGFloat = 0
+        var green: CGFloat = 0
+        var blue: CGFloat = 0
+        
+        if redSwitch.isOn {
+            red = 1
+        }
+        
+        if greenSwitch.isOn {
+            green = 1
+        }
+        
+        if blueSwitch.isOn {
+            blue = 1
+        }
+        
+        let color = UIColor(red: red, green: green, blue: blue, alpha: 1)
+        colorView.backgroundColor = color
+        
+    }
+    
+    
+    
+    
+    
 
 }
 
